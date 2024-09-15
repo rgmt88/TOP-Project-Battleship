79bf07f4d182c8be298f2700b6653d333a299117
rgmt88 <65685133+rgmt88@users.noreply.github.com>
2024-09-14 20:17:33 -0500

    added docs

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ export default {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(process.cwd(), 'main'),
+    path: path.resolve(process.cwd(), 'docs'),
     clean: true, // Cleans the output directory before each build
   },
   module: {
